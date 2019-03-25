75af4e2219f2060e526bc8c6224fc626069bb4d5
StephenAraka <stevenaraka@gmail.com>
2019-03-25 19:24:11 +0300

    Add files for the Lexical analyzer

---
+++ analyzer.h
@@ -0,0 +1,26 @@
+#define TOKEN_COUNT 0
+#define LEXEME_COUNT 1
+
+int identifiers[2] = {0, 0};
+int integers[2] = {0, 0};
+int floats[2] = {0, 0};
+int literals[2] = {0, 0};
+int keywords[2] = {0, 0};
+int punctuation[2] = {0, 0};
+int unary_operators[2] = {0,0};
+int arithmetic_operators[2] = {0,0};
+int relational_operators[2] = {0,0};
+int assignment_operators[2] = {0,0};
+int logical_operators[2] = {0,0};
+int bitwise_operators[2] = {0,0};
+int left_paren[2] = {0,0};
+int right_paren[2] = {0,0};
+int separator[2] = {0,0};
+int terminator[2] = {0,0};
+int left_curly_bracket[2] = {0,0};
+int right_curly_bracket[2] = {0,0};
+int left_bracket[2] = {0,0};
+int right_bracket[2] = {0,0};
+int comment[2] = {0,0};
+int ternary_question_mark[2] = {0,0};
+int ternary_colon[2] = {0,0};
